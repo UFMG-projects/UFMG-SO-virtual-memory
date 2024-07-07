fe5bfa2ff64eb81bf9b0e4677037704cd125f18b
MariAssisR <81930313+MariAssisR@users.noreply.github.com>
2024-07-07 19:23:32 -0300

    working but not all :)

--- tp2virtual.c
+++ tp2virtual.c
@@ -1,8 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+// struct for the pages
+typedef struct page{
+    unsigned id;
+    struct page *next;
+    struct page *prev;
+    int changed; // ??????????????????????????????????
+}page;
+
+// struct for the page table
+typedef struct page_table{
+    page *first;
+    page *last;
+    int size;
+}page_table;
+
+// initialize global
+page_table *table;
+// initialize variables for report
+int pageFaults = 0; 
+int diskAccess = 0;
+int memoryAccess = 0;
+
+// function prototypes
+int findS_offset(int PageSize);
+void addPage(unsigned addr);
+void updatePage(page* page_to_update);
+page* checkPage(unsigned addr);
+void updateAddress(unsigned addr, char* alg);
+void LRU(unsigned addr);
+void FIFO(unsigned addr);
+void secondChance(unsigned addr);
+void random(unsigned addr);
 
 // find the number of bits for the offset for find page
-int findS(int PageSize){
+int findS_offset(int PageSize){
     int s = 0;
     int tmp = PageSize * 1024;
     while(tmp > 1){
@@ -10,6 +45,87 @@ int findS(int PageSize){
         s++;
     }
     return s;
+}
+
+void addPage(unsigned addr){
+    page *new_page = malloc(sizeof(page));
+    new_page->id = addr;
+    new_page->prev = NULL;
+    new_page->next = NULL;
+
+    if(table->size == 0){
+        table->first = new_page;
+        table->last = new_page;
+    }
+    else if(table->size == 1){
+        table->last = new_page;
+        table->first->next = new_page;
+        new_page->prev = table->first;
+    }
+    else{
+        new_page->prev = table->last;
+        table->last->next = new_page;
+        table->last = new_page;
+    }
+    table->size++;
+}
+
+void updatePage(page* page_to_update){
+    
+}
+
+page* checkPage(unsigned addr){
+    page *aux = table->first;
+    for(int i = 0; i < table->size; i++){
+        if(aux->id == addr)
+            return aux;
+        aux = aux->next;
+    }
+    return NULL;
+}
+
+
+void updateAddress(unsigned addr, char* alg){
+    if(strcmp(alg, "lru") == 0){
+        LRU(addr);
+    }
+    else if(strcmp(alg, "fifo") == 0){
+        FIFO(addr);
+    }
+    else if(strcmp(alg, "2a") == 0){
+        secondChance(addr);
+    }
+    else if(strcmp(alg, "random") == 0){
+        random(addr);
+    }
+}
+
+void LRU(unsigned addr){
+    //TODO
+}
+
+void FIFO(unsigned addr){
+    // 
+    page *aux = table->first;
+    // 
+    table->first = table->first->next;
+    table->first->prev = NULL;
+    //  
+    table->last->next = aux;
+    aux->prev = table->last;
+    aux->next = NULL;
+    aux->id = addr;
+    table->last = aux;
+
+    diskAccess++;
+}
+
+void secondChance(unsigned addr){
+    //TODO
+}
+
+void random(unsigned addr){
+    //TODO
 }
 
 int main(int argc, char* argv[]){
@@ -21,22 +137,56 @@ int main(int argc, char* argv[]){
     int memSize = atoi(argv[4]); //128 to 16384 
 
     int numPages = memSize/pageSize;
-    int s = findS(pageSize);
+    int s = findS_offset(pageSize);
 
     // alocate tad to virtual memory
-    // TODO
-
-    // initialize global variables
-    // CHANGE THIS TO GLOBAL, IF NECESSARY
-    // TODO: 
-    int pageFaults; 
-    int diskAccess;
+    table = malloc(sizeof(page_table));
+    table->first = NULL;
+    table->last = NULL;
+    table->size = 0;
 
     //file read
     unsigned addr;
     char rw;
     while(fscanf(file,"%x %c",&addr,&rw) != -1){
         addr = addr >> s;
+        memoryAccess++;
+
+        if(tolower(rw) == 'w'){
+            //check if space in page table
+            if(numPages > table->size){
+                // check if page exist
+                if(!checkPage(addr))
+                    addPage(addr);    
+                else{
+                    // do nothing
+                    continue;
+                }
+            }
+            else{
+                // out of space to add
+                // remove page and add new
+                //use algoritms
+                updateAddress(addr, alg);
+            }
+            
+        }
+        else if(tolower(rw) == 'r'){
+            // check if page exist
+            if(!checkPage(addr)){
+                // faults
+                // add page
+                pageFaults++;
+                if(numPages > table->size)
+                    addPage(addr);
+                else
+                    updateAddress(addr, alg);
+            }
+            else{
+                // just read
+                continue;
+            }
+        }
     }
 
 
